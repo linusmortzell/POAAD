4ca204f67e049adff0dc850993085d900803a93b
linusmortzell <lm222qm@student.lnu.se>
2019-08-27 16:16:24 +0200

    Minor changes again

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -7,8 +7,6 @@
 
 'use strict'
 
-console.log('hej hej')
-
 var map = new ol.Map({
   target: 'map',
   layers: [
@@ -21,6 +19,33 @@ var map = new ol.Map({
     zoom: 12
   })
 })
+
+navigator.geolocation.getCurrentPosition(function(position) {       
+  document.getElementById('anzeige').innerHTML="Latitude: " + position.coords.latitude + "   Longitude: " +
+  position.coords.longitude + "<p>";
+  var lonLat = new OpenLayers.LonLat(position.coords.longitude,
+    position.coords.latitude)
+    .transform(
+      new OpenLayers.Projection("EPSG:4326"), //transform from WGS 1984
+      map.getProjectionObject() //to Spherical Mercator Projection
+    );
+                                  
+  markers.addMarker(new OpenLayers.Marker(lonLat));
+ 
+  map.setCenter(lonLat, 14 // Zoom level
+  );
+ 
+});
+
+map.setCenter(new
+OpenLayers.LonLat(3,3) // Center of the map
+  .transform(
+    new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
+    new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator Projection
+  ), 15 // Zoom level
+);
+var markers = new OpenLayers.Layer.Markers( "Markers" );
+map.addLayer(markers);
 
 /* var isobaths = new ol.layer.Image({
   source: new ol.source.ImageWMS({
