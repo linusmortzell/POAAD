b26912dd6381aa9c1abb1e6cf27df14ec246a8f2
linusmortzell <lm222qm@student.lnu.se>
2019-09-04 16:19:34 +0200

    Adding OSM

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -7,45 +7,50 @@
 
 'use strict'
 
-var map = new ol.Map({
-  target: 'map',
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM()
-    })
-  ],
-  view: new ol.View({
-    center: ol.proj.fromLonLat([18.02233, 64.83165]),
-    zoom: 12
-  })
-})
+var markers = new OpenLayers.Layer.Markers('Markers')
+var map = new OpenLayers.Map('map')
+var zoom = 16
 
-navigator.geolocation.getCurrentPosition(function(position) {       
-  document.getElementById('anzeige').innerHTML="Latitude: " + position.coords.latitude + "   Longitude: " +
-  position.coords.longitude + "<p>";
-  var lonLat = new OpenLayers.LonLat(position.coords.longitude,
-    position.coords.latitude)
-    .transform(
-      new OpenLayers.Projection("EPSG:4326"), //transform from WGS 1984
-      map.getProjectionObject() //to Spherical Mercator Projection
-    );
-                                  
-  markers.addMarker(new OpenLayers.Marker(lonLat));
- 
-  map.setCenter(lonLat, 14 // Zoom level
-  );
- 
-});
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(success, error)
+} else {
+  alert('Your browser doesn\'t support geolocation')
+}
 
-map.setCenter(new
-OpenLayers.LonLat(3,3) // Center of the map
-  .transform(
-    new OpenLayers.Projection("EPSG:4326"), // transform from WGS 1984
-    new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator Projection
-  ), 15 // Zoom level
-);
-var markers = new OpenLayers.Layer.Markers( "Markers" );
-map.addLayer(markers);
+function success (position) {
+  var lon = position.coords.longitude
+  var lat = position.coords.latitude
+
+  map.addLayer(new OpenLayers.Layer.OSM())
+
+  var lonLat = OSMlonLat(lon, lat)
+
+  map.addLayer(markers)
+  markers.addMarker(new OpenLayers.Marker(lonLat))
+  map.setCenter(lonLat, zoom)
+
+  // navigator.geolocation.watchPosition(updatePos, error)
+}
+
+function error (error) {
+  alert('Sorry, an error occured: ' + error)
+}
+
+function updatePos (pos) {
+  
+}
+
+function OSMlonLat (lon, lat) {
+  var lonLat = new OpenLayers.LonLat( lon, lat).transform(
+    new OpenLayers.Projection('EPSG:4326'), // transform from WGS 1984
+    map.getProjectionObject() // to Spherical Mercator Projection
+  )
+  return lonLat
+}
+
+function getPosition () {
+  navigator.geolocation.getCurrentPosition(success, error)
+}
 
 /* var isobaths = new ol.layer.Image({
   source: new ol.source.ImageWMS({
